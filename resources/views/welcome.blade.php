1ff3731f21911c67d77d80e25ff7df89170a287b
Daniel Monjezi (U2066800) <U2066800@unimail.hud.ac.uk>
2022-11-21 13:04:41 +0000

    Add latest visitors to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.app')
 
+@section('title', 'Latest visitors')
+@section('content')
 
-@section('title', 'Welcome to the sign it visitor book')
+@foreach ($latestVisitors as $visitor)
+
+    <article>
+        <h3><a href="{{ route('visitors.show', $visitor->id) }}">{{ $visitor->user->name }}</a></h3>
+        <p>{{ $visitor->comments }}</p>
+    </article>
+@endforeach
+
+@endsection
+
